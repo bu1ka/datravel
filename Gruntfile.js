c09f1990c096cb231c69358d718f81d334166f8d
Alexey Solomentsev <buu1ka@yandex.ru>
2014-05-21 17:35:20 +0400

    improoooving

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,28 @@ module.exports = function(grunt) {
                     out: "js/build.js"
                 }
             }
+        },
+        less: {
+            development: {
+                files: {
+                    "build/css/main.css": "css/less/*"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.less',
+                tasks: ['less']
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    //grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-    grunt.registerTask('default', ['requirejs']);
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less']);
 
 };
